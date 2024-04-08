00fdc5f13284abfc448c8970cb57373ae52de9e1
Mateusz Bodziak <external.bodziak_mateusz@allianzdirect.de>
2024-04-08 08:36:38 +0200

    Reformat

--- transformer/gpt.py
+++ transformer/gpt.py
@@ -1,9 +1,10 @@
-import torch
-import torch.nn as nn
-from torch.nn import functional as F
-from model import GPTLanguageModel, loadModel, saveModel,block_size, batch_size,device
-from pathlib import Path
 import os
+from pathlib import Path
+
+import torch
+
+from model import GPTLanguageModel, loadModel, saveModel, block_size, batch_size, device
+
 
 ################
 @torch.no_grad()
@@ -20,11 +21,12 @@ def estimate_loss():
     model.train()
     return out
 
+
 # Run time parameters
 input_file_name = 'shakespeare.txt'
 model_folder = 'shakespeare'
 max_iters = 4000
-saved_model_name = model_folder+'.pth'
+saved_model_name = model_folder + '.pth'
 mode = 'train'
 # mode='load'
 # learning params
@@ -34,37 +36,36 @@ learning_rate = 3e-4
 ################
 
 
-
 # Paths
 current_dir = os.path.dirname(os.path.abspath(__file__))
 input_file_path = os.path.join(current_dir, input_file_name)
 saved_model_path = os.path.join(current_dir, model_folder, saved_model_name)
 
-print("Runnunig on: ",device)
-print("Input file path: ",input_file_path)
-print("Saved model path: ",saved_model_path)
+print("Runnunig on: ", device)
+print("Input file path: ", input_file_path)
+print("Saved model path: ", saved_model_path)
 ################
 # Load the input
 with open(input_file_path, 'r', encoding='utf-8') as f:
     text = f.read()
 
 # instantiate the model
-vocabulary=[]
+vocabulary = []
 if Path(saved_model_path).exists():
-    print("Found a model and loading it for further trainning.")
-    model,vocabulary  = loadModel(saved_model_path)
-    vocab_size=len(vocabulary)
+    print("Found a model and loading it for further usage.")
+    model, vocabulary = loadModel(saved_model_path)
+    vocab_size = len(vocabulary)
 else:
     # here are all the unique characters that occur in this text
     vocabulary = sorted(list(set(text)))
     vocab_size = len(vocabulary)
-    print("Number of possible tokens: "+str(vocab_size)) 
+    print("Number of possible tokens: " + str(vocab_size))
     # create a mapping from characters to integers
     print("Creating a new model.")
-    model= GPTLanguageModel(vocab_size)   
+    model = GPTLanguageModel(vocab_size)
 
 # encode and decode functions for strings to tensors 
-print("Vocabulary size: ",vocab_size)
+print("Vocabulary size: ", vocab_size)
 stoi = {ch: i for i, ch in enumerate(vocabulary)}
 itos = {i: ch for i, ch in enumerate(vocabulary)}
 encode = lambda s: [stoi[c] for c in s]  # encoder: take a string, output a list of integers
@@ -76,6 +77,7 @@ n = int(0.9 * len(data))  # first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
 
+
 def get_batch(split):
     # generate a small batch of data of inputs x and targets y
     data = train_data if split == 'train' else val_data
@@ -84,15 +86,17 @@ def get_batch(split):
     y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
     x, y = x.to(device), y.to(device)
     return x, y
+
+
 ################
 
-if(mode=='train'):
+if (mode == 'train'):
     m = model.to(device)
     # create a PyTorch optimizer
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
     # print the number of parameters in the model
     print(sum(p.numel() for p in m.parameters()) / 1e6, 'M parameters')
-    print("The number of learing steps:",max_iters)
+    print("The number of learing steps:", max_iters)
     for iter in range(max_iters):
 
         # every once in a while evaluate the loss on train and val sets
@@ -112,8 +116,7 @@ if(mode=='train'):
         optimizer.step()
 
     saveModel(model, vocabulary, model_folder, saved_model_name)
-            
+
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
 print(decode(model.generate(context, max_new_tokens=500)[0].tolist()))
-
